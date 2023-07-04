31317888d875686c7d12e54a49ec6dcb0838eb5d
Amirkaramat <amir_karamat@yahoo.com>
2023-07-04 10:57:24 +0330

    add backend to project

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+      <div className="bg-gradient-to-t bg-blue-300">Hello world</div>
+  );
+}
+
+export default App;
---
+++ client/src/routes.jsx
@@ -0,0 +1,7 @@
+import Home from './pages/home/Home'
+
+const routes = [
+    {path:"/",element:<Home/>}
+]
+
+export default routes
