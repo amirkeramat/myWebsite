6c044dbaac5ced482815a62a4607ba8a45bbb658
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 12:51:08 +0330

    update route

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,11 +4,11 @@ import Services from './pages/services/Services'
 import Projects from './pages/projects/Projects';
 import Contact from './pages/contact/Contact';
 const routes = [
-  { path: "/", element: <Home /> },
-  { path: "/about", element: <About /> },
-  { path: "/services", element: <Services /> },
-  { path: "/projects", element: <Projects /> },
-  { path: "/contact", element: <Contact /> },
+  { path: "", element: <Home /> },
+  { path: "about", element: <About /> },
+  { path: "services", element: <Services /> },
+  { path: "projects", element: <Projects /> },
+  { path: "contact", element: <Contact /> },
 ];
 
 export default routes
