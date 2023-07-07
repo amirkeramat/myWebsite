7306d6aa19f562ecd4f64349bb7352446e3a298e
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 09:43:04 +0330

    add aos function to run

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
 import Layout from "./layout/Layout";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 function App() {
   const router = useRoutes(routes);
   return <Layout>{router}</Layout>;
