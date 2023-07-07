936fbc120c2da17ea0f1bbc825ec5d8e5f0b7c04
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 09:48:37 +0330

    Update routes.jsx

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,7 +1,14 @@
 import Home from './pages/home/Home'
-
+import About from './pages/about/About'
+import Services from './pages/services/Services'
+import Projects from './pages/projects/Projects';
+import Contact from './pages/contact/Contact';
 const routes = [
-    {path:"/",element:<Home/>}
-]
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/services", element: <Services /> },
+  { path: "/projects", element: <Projects /> },
+  { path: "/contact", element: <Contact /> },
+];
 
 export default routes
