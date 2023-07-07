2e96e0887ba42f24211a5ce90ca3eccdcd15b72a
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 09:48:26 +0330

    Update Services.jsx

--- client/src/pages/services/Services.jsx
+++ client/src/pages/services/Services.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Content from "../../components/services/Content"
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
+    <div className='w-full h-full flex  items-center justify-center pb-10'>
+      <Content />
+    </div>
+  );
 }
 
 export default Services
