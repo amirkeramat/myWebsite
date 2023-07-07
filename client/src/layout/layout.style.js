6636ea084491a30bd3768ac474511033e7f50843
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 09:47:23 +0330

    Update Layout component

--- client/src/layout/layout.style.js
+++ client/src/layout/layout.style.js
@@ -11,3 +11,10 @@ fixed top-0 left-0 z-50 bg-black w-full md:block lg:h-4 xl:h-8
 export const BottomSep = tw.div`
 fixed bottom-0 left-0 z-50 bg-black w-full md:block lg:h-4 xl:h-8
 `
+
+export const RouterContainer = tw.div`
+relative w-full max-w-screen-xl 
+`;
+
+export const Router = tw.div`
+relative  bg-gradient-to-tr from-zinc-900 via-zinc-900 to-zinc-800  w-full lg:pt-4 xl:pt-8  rounded h-full`;
