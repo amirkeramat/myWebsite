d8c70b41ec80a6adef0bd29147f499233d26978c
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 09:45:11 +0330

    Create Menu.jsx and menu.style.js

---
+++ client/src/components/layout/menu/Menu.jsx
@@ -0,0 +1,56 @@
+import { menuItems } from "../../../constants/menuItems";
+import { Container, Icon } from "./menu.style";
+import { NavLink } from "react-router-dom";
+import { MoreOutlined } from "@ant-design/icons";
+import { useState } from "react";
+const Menu = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <Container>
+      {menuItems.map((item) => (
+        <Icon
+          onClick={() =>
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+          }
+          $exLink={item.exLink === 1}
+          key={item.id}
+          after-dynamic-value={item.title}>
+          <NavLink
+            onClick={() =>
+              window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+            }
+            className={({ isActive }) =>
+              isActive
+                ? "bg-lime-400 w-full h-full flex justify-center items-center"
+                : " w-full h-full flex justify-center items-center"
+            }
+            to={item.href}>
+            {item.icon}
+          </NavLink>
+        </Icon>
+      ))}
+      <i
+        onClick={() => setToggle((prv) => !prv)}
+        className='w-full flex md:hidden justify-center items-center h-full text-3xl'>
+        <MoreOutlined />
+      </i>
+
+      <div
+        className={`absolute bottom-12 right-0 duration-500 ease-out bg-zinc-700  flex md:hidden flex-col items-center justify-center w-[50px] ${
+          toggle ? "max-h-auto h-[130px]" : "max-h-0 h-0"
+        } overflow-hidden`}>
+        {menuItems
+          .filter((item) => item.exLink === 1)
+          .map((item) => (
+            <i
+              key={item.id}
+              className='text-2xl w-full h-full flex  items-center justify-center'>
+              {item.icon}
+            </i>
+          ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Menu;
---
+++ client/src/components/layout/menu/menu.style.js
@@ -0,0 +1,11 @@
+import tw from "tailwind-styled-components";
+
+export const Container = tw.div`
+fixed  right-0 bottom-0  z-40 md:sticky md:h-[50vh] h-[50px] md:top-[25vh] bg-gradient-to-tr from-zinc-900 via-zinc-800 to-zinc-700 md:w-[50px] w-full text-white flex flex-row md:flex-col items-center justify-evenly
+`;
+
+export const Icon = tw.i`
+relative  hover:after:hidden  hover:after:content-[attr(after-dynamic-value)] hover:after:w-[100px] hover:after:h-[20px] hover:after:absolute hover:after:md:flex hover:after:justify-center hover:after:items-center hover:after:py-5 hover:after:rounded hover:after:-left-28 hover:after:top-0 hover:after:bg-blue-700 text-3xl hover:after:text-xl h-full w-full flex justify-center items-center hover:bg-blue-500 transition-all duration-500  hover:after:transition-all hover:after:duration-300 hover:after:animate-pulse
+aria-[current=page]:text-blue-400
+${(p) => (p.$exLink ? "bg-zinc-600 hidden md:flex" : "")}
+`;
