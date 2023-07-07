e2b08d0d20a783d9d3263bc5599e64c954fd0e81
Amirkaramat <amir_karamat@yahoo.com>
2023-07-07 11:51:30 +0330

    fix vit.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    host:true
-  }
-})
+  server: {
+    port: 3000,
+    host: true,
+  },
+  base: /<myWebsite>/,
+});
