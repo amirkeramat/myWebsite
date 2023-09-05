403a0e6965dcaf5014e597c3cf2aa1687b6911d0
Amirkaramat <amir_karamat@yahoo.com>
2023-09-05 11:50:12 +0330

    update resume

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 export const ROOT_URL = "https://cute-pear-swordfish-boot.cyclic.app";
 
 export const EN_RESUME =
-  "https://cvbuilder.me/Builder/Pdf/en/Template11/7981b944-2d40-4d81-a044-c1da8c2d7be0/MyResume-846[www.cvbuilder.me]584.pdf";
+  "https://drive.google.com/file/d/1yraaad3ASSgtasaPsLhbLu6fHyjQTYeu/view?usp=drive_link";
 
 export const PER_RESUME =
-  "https://cvbuilder.me/Builder/Pdf/fa/Template11/7981b944-2d40-4d81-a044-c1da8c2d7be0/MyResume-796[www.cvbuilder.me]452.pdf";
+  "https://drive.google.com/file/d/1f2cftusDv8-opmgC5z2UtHle3icVHwCz/view?usp=drive_link";
